caee44457f556b13ac42d55ceaafdcca90db6c5f
Jose Antonio Rodriguez Flores <233398@ids.upchiapas.edu.mx>
2026-02-20 21:59:33 -0600

    Libs

--- app/src/main/java/com/josetoanto/horoscope/MainActivity.kt
+++ app/src/main/java/com/josetoanto/horoscope/MainActivity.kt
@@ -3,22 +3,15 @@ package com.josetoanto.horoscope
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.josetoanto.horoscope.core.di.AppContainer
-import com.josetoanto.horoscope.features.horoscope.di.HoroscopeModule
 import com.josetoanto.horoscope.features.horoscope.presentation.horoscope.screen.HoroscopeScreen
-import com.josetoanto.horoscope.features.horoscope.presentation.horoscope.viewmodel.HoroscopeViewModelFactory
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private lateinit var appContainer: AppContainer
-    private lateinit var horoscopeModule: HoroscopeModule
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        appContainer = AppContainer(this)
-        horoscopeModule = HoroscopeModule(appContainer)
         setContent {
-            val factory: HoroscopeViewModelFactory = horoscopeModule.provideHoroscopeViewModelFactory()
-            HoroscopeScreen(factory)
+            HoroscopeScreen()
         }
     }
 }
